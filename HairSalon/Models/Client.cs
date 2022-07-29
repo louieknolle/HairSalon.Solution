80e7ba598d3e275142651ec2dd8858875ffb8ede
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-07-28 21:09:33 -0400

    add client model

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -5,6 +5,10 @@ namespace HairSalon.Models
 {
   public class Client 
   {
-    
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public DateTime AppointmentDate { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
   }
 }
