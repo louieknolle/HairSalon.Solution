145bc9a0252f6cf556f3a875d701f10e58b811c9
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-07-29 21:18:43 -0400

    write stylist details control & view

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -34,5 +34,11 @@ namespace HairSalon.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+
   }
 }
