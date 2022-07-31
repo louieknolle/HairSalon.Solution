d851977b856cd91a958df748269312138d16144c
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-07-31 10:41:29 -0400

    fix gallery link, background img and new client btn

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -12,5 +12,11 @@ namespace HairSalon.Controllers
         {
             return View();
         }
+
+        [HttpGet("/Gallery")]
+        public ActionResult Gallery()
+        {
+            return View();
+        }
     }
 }
